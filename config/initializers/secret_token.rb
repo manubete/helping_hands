11f56f8ff07262afc596f6a70b8b8e2a3ae75b63
Apprentice <apprentice@dbc36.local>
2014-05-01 17:37:53 -0700

    rails skeleton with models, migrations, and database.yml set up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PolarBear::Application.config.secret_token = '4615a30289a81869953ebddd4f622b75e04b3d292d7916e41cfc9eba38e26c1631cd50e051d4ea47c3f770d13c102d78726709be82c7fd6c851ba42f019be007'

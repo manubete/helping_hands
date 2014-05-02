72ad7f69fc458f1e4039797bcf55d52b1e1f6c73
Apprentice <apprentice@dbc26.local>
2014-05-01 23:52:56 -0700

    finished getting dynamic rooms

--- app/assets/javascripts/linkChatRoom.js
+++ app/assets/javascripts/linkChatRoom.js
@@ -1,35 +1,27 @@
+function RoomLinker(){
+  var roomPath = $(location).attr('pathname').split('/').pop();
+  this.roomAddress = 'https://luminous-fire-2873.firebaseio.com/room' + roomPath
+  this.firebaseServer = new Firebase(this.roomAddress)
+}
 
+RoomLinker.prototype = {
 
-$(document).ready(function() {
-
-
-
-
-  // $('.room_info').on('click', function(event) {
-  //   this.roomNumber = $(event.target).data('temp')
-  // })
-
-
-    messagesRef = new Firebase('https://luminous-fire-2873.firebaseio.com/room3')
+  bindButtons: function() {
+    self = this;
     $('#messageInput').keypress(function (e) {
-    if (e.keyCode == 13) {
-      var name = $('#nameInput').val();
-      var text = $('#messageInput').val();
-      messagesRef.push({name:name, text:text});
-      $('#messageInput').val('');
-    }
-  });
-  messagesRef.limit(10).on('child_added', function (snapshot) {
-    var message = snapshot.val();
-    $('<div/>').text(message.text).prepend($('<em/>')
+      if (e.keyCode == 13) {
+        var name = $('#nameInput').val();
+        var text = $('#messageInput').val();
+        self.firebaseServer.push({name:name, text:text});
+        $('#messageInput').val('');
+      }
+    });
+
+    self.firebaseServer.limit(10).on('child_added', function (snapshot) {
+      var message = snapshot.val();
+      $('<div/>').text(message.text).prepend($('<em/>')
       .text(message.name+': ')).appendTo($('#messagesDiv'));
-    $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
-  });
-  // }
-
-
-})
-
-
-
-
+      $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
+    });
+  }
+}

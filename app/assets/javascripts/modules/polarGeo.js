6371e79c58115b075b3bae7d7641c0d3a9b0366e
Apprentice <apprentice@dbc36.local>
2014-05-02 17:19:22 -0700

    Finished MVP GeoHelper Module     *Fully Tested     *Coded as a JS Module

--- app/assets/javascripts/modules/polarGeo.js
+++ app/assets/javascripts/modules/polarGeo.js
@@ -7,6 +7,40 @@ var geoHelper = (function(){
     return text;
   }
 
+  var _calculateDistance = function distance(lat1, lon1, lat2, lon2) {
+    var radlat1 = Math.PI * lat1/180
+    var radlat2 = Math.PI * lat2/180
+    var radlon1 = Math.PI * lon1/180
+    var radlon2 = Math.PI * lon2/180
+    var theta = lon1-lon2
+    var radtheta = Math.PI * theta/180
+    var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+    dist = Math.acos(dist)
+    dist = dist * 180/Math.PI
+    dist = dist * 60 * 1.1515
+    return dist
+
+  }
+
+  var _inRange = function (location1, location2) {
+
+    var acceptable_range = .5 // this is the range variable to set
+
+    var lat1 = location1[0]
+    var lat2 = location2[0]
+    var lon1 = location1[1]
+    var lon2 = location2[1]
+
+    var distance = _calculateDistance(lat1, lon1, lat2, lon2)
+
+    if (distance < acceptable_range){
+      return true
+    } else {
+      return false
+    }
+
+  }
+
   var _default = { enableHighAccuracy: true, timeout: 1000, maximumAge: 0}
   var _success = function(position){
       document.cookie = _randomizeString()
@@ -24,7 +58,9 @@ var geoHelper = (function(){
     failure: _failure,
     success: _success,
     defaultOps: _default,
-    randomizeString: _randomizeString
+    randomizeString: _randomizeString,
+    calculateDistance: _calculateDistance,
+    inRange: _inRange
   }
 }())
 

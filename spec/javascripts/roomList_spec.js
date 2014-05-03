d501b78f22acb0f98699fa8bd800c8cd5d85cf79
arcin <charlesarcin@gmail.com>
2014-05-02 20:42:26 -0700

    set up testing for RoomList model   * set up fixtures.js to mock out objects for easier testing.   * creates returnRooms which returns an array of rooms from Firebase   * passing returnRooms test   * added firebase.js for future mocking

---
+++ spec/javascripts/roomList_spec.js
@@ -0,0 +1,11 @@
+describe("RoomList Thing", function(){
+
+  xit("should access data from Firebase", function(){
+  });
+
+  it("should return the rooms in Firebase", function(){
+    var roomList = new RoomList()
+    var rooms = roomList.returnRooms( database )
+    expect(rooms).toEqual( ['room1', 'room2', 'skippers'] )
+  });
+})

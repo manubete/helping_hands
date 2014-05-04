f710f4df7bcae50e3d2dffa32079beebb5e7ae7b
Charles Arciniega <charlesarcin@gmail.com>
2014-05-03 17:24:40 -0700

    roomListView sends ajax request for the handlebars partial. and compiles it into html on success

--- app/assets/javascripts/modules/polarGeo.js
+++ app/assets/javascripts/modules/polarGeo.js
@@ -44,13 +44,13 @@ var geoHelper = (function(){
   var _default = { enableHighAccuracy: true, timeout: 1000, maximumAge: 0}
   var _success = function(position){
       document.cookie = _randomizeString()
-      $.ajax({
-        url: '/users#create',
-        type: 'POST',
-        data: { user_token: document.cookie, lat: position.coords.latitude, lon: position.coords.longitude }
-      })
-      .done(function(){console.log("ajax was goooood")}) //BUGBUG
-      .fail(function(){console.log("ajax was baaaaaad")}) //BUGBUG
+      // $.ajax({
+      //   url: '/users#create',
+      //   type: 'POST',
+      //   data: { user_token: document.cookie, lat: position.coords.latitude, lon: position.coords.longitude }
+      // })
+      // .done(function(){console.log("ajax was goooood")}) //BUGBUG
+      // .fail(function(){console.log("ajax was baaaaaad")}) //BUGBUG
   }
   var _failure = function(position){ return "Sorry, we couldn't find you" }
 

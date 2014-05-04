f790605c1c7a6934d42ace21c0dbb30d348d353f
keopaseutb <keopaseutb01@hotmail.com>
2014-05-04 15:02:57 -0700

    cookie factory is now in the module

--- db/schema.rb
+++ db/schema.rb
@@ -15,18 +15,12 @@ ActiveRecord::Schema.define(:version => 20140502003155) do
 
   create_table "rooms", :force => true do |t|
     t.string   "name"
-    t.float    "lat"
-    t.float    "lon"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
 
   create_table "users", :force => true do |t|
-    t.integer  "room_id"
     t.string   "name"
-    t.string   "user_token"
-    t.float    "lat"
-    t.float    "lon"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end

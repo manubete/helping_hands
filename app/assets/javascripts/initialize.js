4675989ce39bde5bddbb367db7bed89ed4f02a2c
Rodrigo Manubens <manubete@Rodrigos-MacBook-Pro-2.local>
2014-05-04 18:19:01 -0700

    cleaned the leftover code

--- app/assets/javascripts/initialize.js
+++ app/assets/javascripts/initialize.js
@@ -51,7 +51,7 @@ PolarBear = {
     var room = new Room(chatRoomUrl)
     var roomController = new RoomController(room, roomView)
 
-    roomView.controller = roomController
+
     //creates a JSON object to insert a name into the room
      var roomName = {name: roomController.model.chatRoomUrl}
 
--- spec/javascripts/geoHelper_spec.js
+++ spec/javascripts/geoHelper_spec.js
@@ -1,18 +1,5 @@
 describe('geoHelper', function(){
-  it("creates a string that is 15 characters long", function(){
-    expect(geoHelper.randomizeString().length).toEqual(15)
-  });
 
-  it("creates a random string", function(){
-    var count = 0
-    var strings = []
-    while(count < 10) {
-      strings.push(geoHelper.randomizeString())
-      count++
-    }
-
-    expect(strings).not.toContain(geoHelper.randomizeString())
-  })
 
   it("calculates the distance between two geo points", function(){
     var location1 = [37.780446, -122.405950]

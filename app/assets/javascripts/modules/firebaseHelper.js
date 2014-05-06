482dcb2935316ea63e33ae501412467962989d24
keopaseutb <keopaseutb01@hotmail.com>
2014-05-05 21:45:02 -0700

    adds grid styles to chatroom. need to fix input box

--- app/assets/javascripts/modules/firebaseHelper.js
+++ app/assets/javascripts/modules/firebaseHelper.js
@@ -44,7 +44,7 @@ var firebaseHelper = (function() {
     var chatRoom = firebaseServer
     chatRoom.limit(10).on('child_added', function (snapshot) {
       var message = snapshot.val();
-      $('<div>').text(message.user_token+': '+message.message).fadeIn().appendTo($('#messagesDiv'));
+      $('<div class="elevencol">').text(message.user_token+': '+message.message).fadeIn().appendTo($('#messagesDiv'));
       $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
     })
   }

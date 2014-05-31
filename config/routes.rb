aab075db7ae0207fb6bcf8186351584142ff6be1
Rodrigo Manubens <manubete@Rodrigos-MacBook-Pro-2.local>
2014-05-31 14:13:26 -0700

    cleaning up database and files still

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 PolarBear::Application.routes.draw do
 
   #routes and roads wont be there, in promise specify how the help can arrive
-  root to: "rooms#index"
-  resources :rooms, only: [:show, :index]
-  resources :chatrooms, only: [:show]
+  root to: "promises#index"
+  resources :promises
 
-  get '/room_list', to: 'rooms#room_list'
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140506035654) do
+ActiveRecord::Schema.define(:version => 20140531205402) do
+
+  create_table "promises", :force => true do |t|
+    t.string  "organization"
+    t.string  "resource"
+    t.integer "resource_count"
+    t.string  "address"
+    t.string  "description"
+    t.string  "disaster"
+  end
 
 end

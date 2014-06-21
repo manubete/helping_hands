1272de06dca9af6246c336be616fad189a66a8f5
Rodrigo Manubens <manubete@unknowna8bbcf206a74.att.net>
2014-06-21 16:52:35 -0700

    - Added the background image and logo to the asset pipeline - Added a header and footer to the layout to refactor the view HTML code

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ PolarBear::Application.routes.draw do
 
   #routes and roads wont be there, in promise specify how the help can arrive
   root to: "requests#index"
+
   resources :requests do
     collection do
       get :list

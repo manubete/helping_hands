514f236e7a19f90cf1b8acf000c0a2a60214fb36
Rodrigo <rsm2165@columbia.edu>
2014-07-14 20:35:40 -0700

    Added some of the new db fields to the factory girl file

--- spec/controllers/factories.rb
+++ spec/controllers/factories.rb
@@ -3,6 +3,7 @@ FactoryGirl.define do
   factory :donor do
     name "Erin"
     city "San Francisco"
+    address "Lovely dovely Street"
     phone_number "555-555-5555"
     email "erin@erin.com"
     username "ErinJoan"
@@ -12,6 +13,9 @@ FactoryGirl.define do
   factory :organization do
     name "Sample Org"
     city "San Francisco"
+    address "Lovely Org Street"
+    operation_days "M-Th"
+    operation_hours "9 to 5"
     phone_number "555-555-5555"
     email "name@sample.org"
     website_url "http://www.google.com"
@@ -24,8 +28,12 @@ FactoryGirl.define do
 
   factory :request do
     organization "Test Organization"
+    organization_id 2
+    start_date Date.today
+    end_date Date.today.next_day
     resource "bottled water"
-    resource_count "100"
+    resource_count 100
+    complete false
     address "100 Fake Street"
     description "Refugees without clean water supply"
   end

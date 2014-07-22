fb5caf1a40f6aaf4f3f9598f3cb5f9943e094ed7
Erin Joan Snyder <Erin.Joan.Snyder@gmail.com>
2014-07-22 16:42:08 -0700

    a user can log in as a donor or an organization

---
+++ spec/features/user_login_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+feature 'Registered user' do
+  scenario 'should be able to see the login button when logged out' do
+    visit root_path
+    expect(page).to have_content('Log In')
+  end
+
+  scenario 'should be able to log in with valid params' do
+    FactoryGirl.create(:donor)
+    log_in_with 'erin@erin.com', 'erinisgreat'
+    expect(page).to have_content("successful login!!")
+  end
+
+  scenario 'should not be able to log in with invalid params' do
+    log_in_with 'invalid_email', 'test'
+    expect(page).to have_content('Unsuccessful login')
+  end
+
+  scenario 'should not be able to log in with blank password' do
+    log_in_with 'test@test.com', ''
+    expect(page).to have_content('Unsuccessful login')
+  end
+
+  def log_in_with(email, password)
+    visit user_type_login_confirmation_sessions_path
+    fill_in 'Email', with: email
+    fill_in 'Password', with: password
+    click_button 'Log In'
+  end
+end

d1cd92a15bb8d6fd142281f02e17cf2f8f4c2004
Erin Joan Snyder <Erin.Joan.Snyder@gmail.com>
2014-07-22 18:46:03 -0700

    a registered organization can log in and view its profile

--- spec/features/user_login_spec.rb
+++ spec/features/user_login_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-feature 'Registered user' do
+feature 'Registered donor' do
   scenario 'should be able to see the login button when logged out' do
     visit root_path
     expect(page).to have_content('Log In')
@@ -31,6 +31,48 @@ feature 'Registered user' do
     click_on 'PROFILE'
     expect(page).to have_content('Location')
     expect(page).to have_content('San Francisco')
+    expect(page).to have_content('Active Requests')
+    expect(page).to have_content('Request History')
+  end
+
+  def log_in_with(email, password)
+    visit user_type_login_confirmation_sessions_path
+    fill_in 'Email', with: email
+    fill_in 'Password', with: password
+    click_on 'Log In'
+  end
+end
+
+feature 'Registered organization' do
+  scenario 'should be able to see the login button when logged out' do
+    visit root_path
+    expect(page).to have_content('Log In')
+  end
+
+  scenario 'should be able to log in with valid params' do
+    FactoryGirl.create(:organization)
+    log_in_with 'name@sample.org', 'password'
+    expect(page).to have_content("successful login!!")
+  end
+
+  scenario 'should not be able to log in with invalid params' do
+    FactoryGirl.create(:organization)
+    log_in_with 'invalid_email', 'password'
+    expect(page).to have_content('Unsuccessful login')
+  end
+
+  scenario 'should not be able to log in with blank password' do
+    FactoryGirl.create(:organization)
+    log_in_with 'name@sample.org', ''
+    expect(page).to have_content('Unsuccessful login')
+  end
+
+  scenario 'should be able to view their profile after logging in' do
+    FactoryGirl.create(:organization)
+    log_in_with 'name@sample.org', 'password'
+    expect(page).to have_content("successful login!!")
+    click_on 'PROFILE'
+    expect(page).to have_content('Make New Request')
     expect(page).to have_content('Active Requests')
     expect(page).to have_content('Request History')
   end

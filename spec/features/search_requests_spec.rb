74a7256b78247b6c7662a6aaa8fc19d98130f0a1
Erin Joan Snyder <Erin.Joan.Snyder@gmail.com>
2014-07-22 18:31:04 -0700

    a user can search for a request, click on the organization and view the request

--- spec/features/search_requests_spec.rb
+++ spec/features/search_requests_spec.rb
@@ -16,14 +16,23 @@ feature 'Logged in user' do
   end
 
   scenario 'can search for a request' do
-    Request.create(organization_id: 1, resource: 'bottled water', current_resource_count: 0, target_resource_count: 100, address: '100 fake street', organization: 'test org', description: 'Refugees without clean water supply', start_date: Date.today, end_date: Date.today.next_day, complete: false, tag_list: ["sdf","sds"])
+    FactoryGirl.create(:request)
     Request.create(organization_id: 2, resource: 'underwear', current_resource_count: 0, target_resource_count: 100, address: 'the castro', organization: 'Underwear Supply Co', description: 'Naked dudes in the Castro need underpants', start_date: Date.today, end_date: Date.today.next_day, complete: false, tag_list: ["sdf","sds"])
 
     visit requests_path
-    fill_in 'search', :with => 'test org'
+    fill_in 'search', :with => 'Test Org'
     click_on 'Search'
-    expect(page).to have_content('100 fake street')
+    expect(page).to have_content('100 Fake Street')
     expect(page).to_not have_content('the castro')
+  end
+
+  scenario 'can click on and view a request' do
+    FactoryGirl.create(:request)
+    FactoryGirl.create(:organization)
+    visit requests_path
+    click_on 'Test Org'
+    expect(page).to have_content('Delivery Address')
+    expect(page).to have_content('Upload a clear photo of your donation')
   end
 
   # TODO: test sorting functionality

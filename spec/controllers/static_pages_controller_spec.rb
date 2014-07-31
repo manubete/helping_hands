d288321078774a57605552383935e02620260276
sailormoon <serena.s.xu@gmail.com>
2014-07-31 14:34:04 -0700

    static links

---
+++ spec/controllers/static_pages_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe StaticPagesController do
+
+  describe "GET 'about'" do
+    it "returns http success" do
+      get 'about'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'questions'" do
+    it "returns http success" do
+      get 'questions'
+      response.should be_success
+    end
+  end
+
+end

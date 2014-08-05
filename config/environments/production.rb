5de77b6883fb425da896874634650e864ffcd2d4
sailormoon <serena.s.xu@gmail.com>
2014-08-04 18:35:27 -0700

    one more

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,7 @@ HelpingHands::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w( layout.css )
+  config.assets.precompile += %w( jquery.ui.widget.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   config.action_mailer.raise_delivery_errors = true

baf7de05501fe7fddfe0e4c9c7ed29146c57e315
mannykary <mrkaryam@gmail.com>
2014-08-09 23:50:05 -0700

    Refactored to make DRY; moved organization form to partial and image upload JS to separate file.

---
+++ app/assets/javascripts/image_upload.js
@@ -0,0 +1,17 @@
+function readURL(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+      $('#img_prev').attr('src', e.target.result);
+    };
+
+    reader.readAsDataURL(input.files[0]);
+  }
+}
+
+$(document).ready(function(){
+  $(".fakeButton").click(function(){
+    $(".actualButton").click();
+  });
+});
